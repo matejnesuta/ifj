c871225ee2a8c6f40431fd2ccc5d7e6aaba1eaf2
stefanpeknik <stefa9982@seznam.cz>
2022-10-10 23:45:00 +0200

    setup enum of states

--- scanner.c
+++ scanner.c
@@ -1,6 +1,57 @@
 #include <stdio.h>
 
-int main() {
+typedef enum 
+{
+    Start,
+    Multiply,
+    Division,
+    LineComment,
+    BlockComment,
+    ExpectEnd,
+    LineCommentEnd,
+    BlockCommentEnd,
+    Whitespace,
+    Plus,
+    Minus,
+    Concat,
+    Assign,
+    ExpectEqual,
+    Equal,
+    Gt,
+    Ge,
+    Lt,
+    Le,
+    ExpectStartProlog1,
+    ExpectStartProlog2,
+    ExpectStartProlog3,
+    StartPrologEnd,
+    Semicolon,
+    LeftBracket,
+    RightBracket,
+    LeftCurlyBracket,
+    RightCurlyBracket,
+    Integer,
+    ExpFloat,
+    IntDotInt,
+    Exponent,
+    PlusMinus,
+    IntAfterExponent,
+    Float,
+    EnableNull,
+    EndPrologEnd,
+    Identifier,
+    Colon,
+    ExclamationMark,
+    ExpectNotEqual,
+    NotEqual,
+    StartQuotes,
+    escapeSeq,
+    Text,
+    EndQuotes
+} FSMstate;
+
+int main() 
+{
     printf("Hello World!\n");
     return 0;
 }
