4c1bff1ac8a8a5fa94d5a973515ddd98666b4fd7
stefanpeknik <stefa9982@seznam.cz>
2022-10-11 01:43:24 +0200

    add switch for FSM transitions
    include transitions from Start state

--- scanner.c
+++ scanner.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 typedef enum 
 {
@@ -39,6 +40,7 @@ typedef enum
     Float,
     EnableNull,
     EndPrologEnd,
+    Variable,
     Identifier,
     Colon,
     ExclamationMark,
@@ -51,7 +53,60 @@ typedef enum
     Error
 } FSMstate;
 
-int main() 
+FSMstate transition(FSMstate currIn, char edge)
+{
+    switch(currIn)
+    {
+        case Start:
+            switch(edge)
+            {
+                case '*':
+                    return Multiply;
+                case '/':
+                    return Division;
+                case '+':
+                    return Plus;
+                case '-':
+                    return Minus;
+                case '.':
+                    return Concat;
+                case '=':
+                    return Assign;
+                case '>':
+                    return Gt;
+                case '<':
+                    return Lt;
+                case ';':
+                    return Semicolon;
+                case '(':
+                    return LeftBracket;
+                case ')':
+                    return RightBracket;
+                case '{':
+                    return LeftCurlyBracket;
+                case '}':
+                    return RightCurlyBracket;
+                case '?':
+                    return EnableNull;
+                case '$':
+                    return Variable;
+                case ':':
+                    return Colon;
+                case '!':
+                    return ExclamationMark;
+                case '"':
+                    return StartQuotes;
+            }
+            if(isspace(edge))
+                return Whitespace;
+            if(isdigit(edge))
+                return Integer;
+            if(isletter(edge) || edge == '_')
+                return Identifier;
+    }
+}
+
+int main()
 {
     printf("Hello World!\n");
     return 0;
