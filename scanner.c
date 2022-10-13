21756ed79131f83ae0047f32b3f68f8787156d50
stefanpeknik <stefa9982@seznam.cz>
2022-10-13 11:09:01 +0200

    finish transition switch of all states of FSM for scanner

--- scanner.c
+++ scanner.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 typedef enum 
 {
     Start,
     Multiply,
     Division,
-    LineComment,
     BlockComment,
-    ExpectEnd,
-    LineCommentEnd,
-    BlockCommentEnd,
-    Whitespace,
+    LineComment,
+    ExpectEndBlockComment,
     Plus,
     Minus,
     Concat,
@@ -25,6 +24,7 @@ typedef enum
     ExpectStartProlog1,
     ExpectStartProlog2,
     ExpectStartProlog3,
+    ExpectStartProlog4,
     StartPrologEnd,
     Semicolon,
     LeftBracket,
@@ -32,78 +32,230 @@ typedef enum
     LeftCurlyBracket,
     RightCurlyBracket,
     Integer,
-    ExpFloat,
-    IntDotInt,
+    FloatIntDotInt,
     Exponent,
     PlusMinus,
-    IntAfterExponent,
-    Float,
+    FloatIntAfterExponent,
     EnableNull,
-    EndPrologEnd,
+    EndPrologEnd1,
+    EndPrologEnd2,
     Variable,
     Identifier,
     Colon,
+    EndOfFile,
     ExclamationMark,
     ExpectNotEqual,
     NotEqual,
-    StartQuotes,
-    escapeSeq,
-    Text,
-    EndQuotes,
+    String,
+    EscapeSeq,
+    StringEnd,
     Error
 } FSMstate;
 
-FSMstate transition(FSMstate currIn, char edge)
+FSMstate transition(FSMstate currIn, int edge)
 {
     switch(currIn)
     {
         case Start:
             switch(edge)
             {
-                case '*':
-                    return Multiply;
-                case '/':
-                    return Division;
-                case '+':
-                    return Plus;
-                case '-':
-                    return Minus;
-                case '.':
-                    return Concat;
-                case '=':
-                    return Assign;
-                case '>':
-                    return Gt;
-                case '<':
-                    return Lt;
-                case ';':
-                    return Semicolon;
-                case '(':
-                    return LeftBracket;
-                case ')':
-                    return RightBracket;
-                case '{':
-                    return LeftCurlyBracket;
-                case '}':
-                    return RightCurlyBracket;
-                case '?':
-                    return EnableNull;
-                case '$':
-                    return Variable;
-                case ':':
-                    return Colon;
-                case '!':
-                    return ExclamationMark;
-                case '"':
-                    return StartQuotes;
+                case '*': return Multiply;
+                case '/': return Division;
+                case '+': return Plus;
+                case '-': return Minus;
+                case '.': return Concat;
+                case '=': return Assign;
+                case '>': return Gt;
+                case '<': return Lt;
+                case ';': return Semicolon;
+                case '(': return LeftBracket;
+                case ')': return RightBracket;
+                case '{': return LeftCurlyBracket;
+                case '}': return RightCurlyBracket;
+                case '?': return EnableNull;
+                case '$': return Variable;
+                case ':': return Colon;
+                case EOF: return EndOfFile;
+                case '!': return ExclamationMark;
+                case '"': return String;
             }
-            if(isspace(edge))
-                return Whitespace;
-            if(isdigit(edge))
-                return Integer;
-            if(isletter(edge) || edge == '_')
-                return Identifier;
+            if(isspace(edge)) return Start;
+            if(isdigit(edge)) return Integer;
+            if(isalpha(edge) || edge == '_') return Identifier; 
+            return Error;
+
+        case Multiply:
+            return Error;
+
+        case Division:
+            if(edge == '*') return BlockComment;
+            if(edge == '/') return LineComment;
+            return Error;
+
+        case LineComment:
+            if(edge != '\n' && edge != EOF) return LineComment;
+            if(edge == '\n') return Start;
+            return Error;
+
+        case BlockComment:
+            if(edge == '*') return ExpectEndBlockComment;
+            return Error;
+
+        case ExpectEndBlockComment:
+            if(edge != '/' && edge != EOF) return BlockComment;
+            if(edge == '/') return Start;
+            return Error;
+
+        case Plus:
+            if(isdigit(edge)) return Integer;
+            return Error;
+
+        case Minus:
+            if(isdigit(edge)) return Integer;
+            return Error;
+
+        case Integer:
+            if(isdigit(edge)) return Integer;
+            if(edge == '.') return FloatIntDotInt;
+            if(edge == 'e' || edge == 'E') return Exponent;
+            return Error;
+
+        case FloatIntDotInt:
+            if(isdigit(edge)) return FloatIntDotInt;
+            if(edge == 'e' || edge == 'E') return Exponent;
+            return Error;
+
+        case Exponent:
+            if(edge == '+' || edge == '-') return PlusMinus;
+            if(isdigit(edge)) return FloatIntAfterExponent;
+            return Error;
+
+        case PlusMinus:
+            if(isdigit(edge)) return FloatIntAfterExponent;
+            return Error;
+
+        case FloatIntAfterExponent:
+            if(isdigit(edge)) return FloatIntAfterExponent;
+            return Error;
+
+        case Concat:
+            return Error;
+
+        case Assign:
+            if(edge == '=') return ExpectEqual;
+            return Error;
+
+        case ExpectEqual:
+            if(edge == '=') return Equal;
+            return Error;
+
+        case Equal:
+            return Error;
+
+        case Gt:
+            if(edge == '=') return Ge;
+            return Error;
+
+        case Ge:
+            return Error;
+
+        case Lt:
+            if(edge == '=') return Le;
+            if(edge == '?') return ExpectStartProlog1;
+            return Error;
+
+        case Le:
+            return Error;
+
+        case ExpectStartProlog1:
+            if(edge == 'p') return ExpectStartProlog2;
+            return Error;
+
+        case ExpectStartProlog2:
+            if(edge == 'h') return ExpectStartProlog3;
+            return Error;
+
+        case ExpectStartProlog3:
+            if(edge == 'p') return ExpectStartProlog4;
+            return Error;
+
+        case ExpectStartProlog4:
+            if(isspace(edge)) return StartPrologEnd;
+            return Error;
+
+        case StartPrologEnd:
+            return Error;
+
+        case Semicolon:
+            return Error;
+
+        case LeftBracket:
+            return Error;
+
+        case RightBracket:
+            return Error;
+
+        case LeftCurlyBracket:
+            return Error;
+
+        case RightCurlyBracket:
+            return Error;
+
+        case EnableNull:
+            if(edge == '>') return EndPrologEnd1;
+            if(edge >= 97 && edge <= 122) return Identifier; // after ? we expect indetifier type, which is always only lower-case 
+            return Error;
+
+        case EndPrologEnd1:
+            if(edge == '\n') return EndPrologEnd2;
+            return Error;
+
+        case EndPrologEnd2:
+            return Error;
+        
+        case Variable:
+            if(isalpha(edge) || edge == '_') return Identifier;
+            return Error;
+        
+        case Identifier:
+            if(isalpha(edge) || isdigit(edge) || edge == '_') return Identifier;
+            return Error;
+        
+        case Colon:
+            return Error;
+        
+        case EndOfFile:
+            return Error;
+        
+        case ExclamationMark:
+            if(edge == '=') return ExpectNotEqual;
+            return Error;
+        
+        case ExpectNotEqual:
+            if(edge == '=') return NotEqual;
+            return Error;
+        
+        case NotEqual:
+            return Error;
+        
+        case String:
+            if(edge > 31 && edge != '\\' && edge != '"') return String;
+            if(edge == '\\') return EscapeSeq;
+            if(edge == '"') return StringEnd;
+            return Error;
+        
+        case EscapeSeq:
+            if(edge > 31) return String;
+            return Error;
+        
+        case StringEnd:
+            return Error;
+
+        case Error:
+            printf("something bad here happened\n");
+            exit(1);
     }
+    return Error;
 }
 
 int main()
