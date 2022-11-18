40270464f856bd0871227daa0d94371b0c9cf26a
StefanPeknik <xpekni01@stud.fit.vutbr.cz>
2022-11-18 20:24:52 +0100

    create graphviz output of the ASTree

--- ASTree.c
+++ ASTree.c
@@ -46,17 +46,108 @@ AST *ASTreeInsertAnotherChild(AST *tree, AST *child) {
   return tree;
 }
 
+const char *GetNonterminalName(nonterminal_kind nonterminal);
+void ASTreePrintChildrenRec(AST *tree, FILE *f);
 void ASTreePrintChildren(AST *tree) {
+  FILE *f = fopen("ASTree.dot", "w");
+  fprintf(f, "digraph G {node [shape = circle; width = 2;];\n");
+  ASTreePrintChildrenRec(tree, f);
+  fprintf(f, "}");
+  fflush(f);
+  fclose(f);
+}
+void ASTreePrintChildrenRec(AST *tree, FILE *f) {
   if (!tree->children) {
     return;
   }
+
   LList_element *child = tree->children->first;
   while (child != NULL) {
     if (child->tree->node->is_terminal) {
-      printf("%s\n", child->tree->node->terminal->code->data);
+      if (!tree->node->is_terminal)
+        fprintf(f, "\"%s\" -> \"%s\";\n",
+                GetNonterminalName(tree->node->nonterminal),
+                child->tree->node->terminal->code->data);
+      else
+        fprintf(f, "\"%s\" -> \"%s\";\n", tree->node->terminal->code->data,
+                child->tree->node->terminal->code->data);
+      if (child->next != NULL) {
+        fprintf(f, "\"%s\" -> \"%s\";\n",
+                child->tree->node->terminal->code->data,
+                child->next->tree->node->terminal->code->data);
+      }
+
+      if (child->tree->node->terminal->kind != endOfFileTer) {
+        // printf("term: %s\n", child->tree->node->terminal->code->data);
+      }
     } else {
-      ASTreePrintChildren(child->tree);
+      fprintf(f, "\"%s\" -> ", GetNonterminalName(tree->node->nonterminal));
+      fprintf(f, "\"%s\";\n",
+              GetNonterminalName(child->tree->node->nonterminal));
+      ASTreePrintChildrenRec(child->tree, f);
     }
     child = child->next;
   }
+}
+
+const char *GetNonterminalName(nonterminal_kind nonterminal) {
+  char *name;
+  switch (nonterminal) {
+    case START:
+      name = "START";
+      return name;
+    case PROG:
+      name = "PROG";
+      return name;
+    case START_PROLOG:
+      name = "START_PROLOG";
+      return name;
+    case END_PROLOG:
+      name = "END_PROLOG";
+      return name;
+    case CODE:
+      name = "CODE";
+      return name;
+    case BODY:
+      name = "BODY";
+      return name;
+    case INNER_SCOPE:
+      name = "INNER_SCOPE";
+      return name;
+    case RETURN_VALUE:
+      name = "RETURN_VALUE";
+      return name;
+    case RETURN_TYPE:
+      name = "RETURN_TYPE";
+      return name;
+    case FUNC_CALL:
+      name = "FUNC_CALL";
+      return name;
+    case FUNC_CALL_ARGS:
+      name = "FUNC_CALL_ARGS";
+      return name;
+    case NEXT_ARG:
+      name = "NEXT_ARG";
+      return name;
+    case ARG:
+      name = "ARG";
+      return name;
+    case LITERAL:
+      name = "LITERAL";
+      return name;
+    case FUNC_DECLARE:
+      name = "FUNC_DECLARE";
+      return name;
+    case FUNC_DECLARE_BODY:
+      name = "FUNC_DECLARE_BODY";
+      return name;
+    case ARG_TYPE:
+      name = "ARG_TYPE";
+      return name;
+    case IF_ELSE:
+      name = "IF_ELSE";
+      return name;
+  }
+  name = "UNKNOWN";
+  return name;
 }
