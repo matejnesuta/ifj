dbb6aae8b125f729f5129c76ab5c21e65987fc28
Ilia Markelov <imarkelo@redhat.com>
2022-12-02 17:20:35 +0100

    Built-In functions without write. Union version

--- symtable.c
+++ symtable.c
@@ -114,7 +114,152 @@ void symtable_insert_func(tSymtable *TableRoot, string key) {
   bst_insert(&TableRoot->root, key.data, dataPtr, datatypeFunc);
 }
 
-// void symtable_insert_builtin_func (tSymtable *);
+void symtable_insert_builtin_func (tSymtable *TableRoot) {
+  bst_node_ptr_t node;
+  tFunction *func;
+
+  ///reads///
+  string reads;
+  SetupString(&reads);
+
+  AddToString(&reads, "reads");
+  symtable_insert_func(TableRoot, reads);
+
+  node = symtable_search(TableRoot->root, reads);
+  func = node->data->func;
+
+  func->declared = true;
+  func->defined = true;
+  
+  ///readi///
+  string readi;
+  SetupString(&readi);
+
+  AddToString(&readi, "readi");
+  symtable_insert_func(TableRoot, readi);
+
+  node = symtable_search(TableRoot->root, readi);
+  func = node->data->func;
+
+  func->declared = true;
+  func->defined = true;
+
+  ///readf///
+  string readf;
+  SetupString(&readf);
+
+  AddToString(&readf, "readf");
+  symtable_insert_func(TableRoot, readf);
+
+  node = symtable_search(TableRoot->root, readf);
+  func = node->data->func;
+
+  func->declared = true;
+  func->defined = true;
+
+  ///write///
+
+
+  ///floatval///
+  string floatval;
+  SetupString(&floatval);
+
+  AddToString(&floatval, "floatval");
+  symtable_insert_func(TableRoot, floatval);
+
+  node = symtable_search(TableRoot->root, floatval);
+  func = node->data->func;
+
+  func->declared = true;
+  func->defined = true;
+  AddToString(&(func->params), "t");
+  SetupString(&(func->paramnames[0])); 
+  AddToString(&(func->paramnames[0]), "t");
+
+  ///intval///
+  string intval;
+  SetupString(&intval);
+
+  AddToString(&intval, "intval");
+  symtable_insert_func(TableRoot, intval);
+
+  node = symtable_search(TableRoot->root, intval);
+  func = node->data->func;
+
+  func->declared = true;
+  func->defined = true;
+  AddToString(&(func->params), "t");
+  SetupString(&(func->paramnames[0]));
+  AddToString(&(func->paramnames[0]), "t");
+
+  ///strlen///
+  string strlen;
+  SetupString(&strlen);
+
+  AddToString(&strlen, "strlen");
+  symtable_insert_func(TableRoot, strlen);
+
+  node = symtable_search(TableRoot->root, strlen);
+  func = node->data->func;
+
+  func->declared = true;
+  func->defined = true;
+  AddToString(&(func->params), "s");
+  SetupString(&(func->paramnames[0]));
+  AddToString(&(func->paramnames[0]), "s");
+
+  ///substring///
+  string substring;
+  SetupString(&substring);
+
+  AddToString(&substring, "substring");
+  symtable_insert_func(TableRoot, substring);
+
+  node = symtable_search(TableRoot->root, substring);
+  func = node->data->func;
+
+  func->declared = true;
+  func->defined = true;
+  AddToString(&(func->params), "sii");
+  SetupString(&(func->paramnames[0]));
+  AddToString(&(func->paramnames[0]), "s");
+  SetupString(&(func->paramnames[1]));
+  AddToString(&(func->paramnames[1]), "i");
+  SetupString(&(func->paramnames[2]));
+  AddToString(&(func->paramnames[2]), "j");
+
+  ///ord///
+  string ord;
+  SetupString(&ord);
+
+  AddToString(&ord, "ord");
+  symtable_insert_func(TableRoot, ord);
+
+  node = symtable_search(TableRoot->root, ord);
+  func = node->data->func;
+
+  func->declared = true;
+  func->defined = true;
+  AddToString(&(func->params), "s");
+  SetupString(&(func->paramnames[0]));
+  AddToString(&(func->paramnames[0]), "c");
+
+  ///chr///
+  string chr;
+  SetupString(&chr);
+
+  AddToString(&chr, "chr");
+  symtable_insert_func(TableRoot, chr);
+
+  node = symtable_search(TableRoot->root, chr);
+  func = node->data->func;
+
+  func->declared = true;
+  func->defined = true;
+  AddToString(&(func->params), "i");
+  SetupString(&(func->paramnames[0]));
+  AddToString(&(func->paramnames[0]), "i");
+}
 
 void symtable_insert_var(tSymtable *TableRoot, string key) {
   tData *var;
